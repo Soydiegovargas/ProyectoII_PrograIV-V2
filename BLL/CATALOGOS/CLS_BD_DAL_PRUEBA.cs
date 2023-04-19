ef0a95d81d85b27c5308f0cd7842e46bf732cd17
Ricardo_Badilla <ricardo.badilla@uamcr.net>
2023-04-18 19:21:00 -0600

    Edicion de un webform CCSS para base de diseno grafico

---
+++ BLL/CATALOGOS/CLS_BD_DAL_PRUEBA.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.CATALOGOS
+{
+    public class CLS_BD_DAL_PRUEBA
+    {
+
+
+    }
+}
--- UX/Pantallas/Formularios/wfrm_Mant_CCSS.aspx.cs
+++ UX/Pantallas/Formularios/wfrm_Mant_CCSS.aspx.cs
@@ -1,17 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
+
 namespace UX.Pantallas.Formularios
 {
     public partial class wfrm_Mant_CCSS : System.Web.UI.Page
     {
+        //LOAD CON METODO DE BASE DE DATOS QUEMADA
         protected void Page_Load(object sender, EventArgs e)
+        {
+          
+           
+        }
+        //LLAMAR  BASE DE DATOS QUEMADA
+      /*
+        private void LlenarGV()
+        {
+            DataTable GV = new DataTable();
+            GV.Columns.AddRange(new DataColumn[] {
+                new DataColumn("COD_AREA",typeof(string)),
+                new DataColumn("NUM_INCAP",typeof(string)),
+                new DataColumn("NOMBRE",typeof(string)),
+                new DataColumn("DIAS",typeof(string)),
+                new DataColumn("MOTIVO",typeof(string))
+                
+            });
+
+            SqlCommand cmd = new SqlCommand("BD_CCSS.SCH_ADMIN.sp_Listar_INCAPACIDADES", PRUEBAS_DATAGRID.Open());
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.HasRows)
+            {
+                while (dr.Read())
+                {
+                    GV.Rows.Add(
+                        dr["COD_AREA"].ToString(),
+                        dr["NUM_INCAP"].ToString(),
+                        dr["NOMBRE"].ToString(),
+                        dr["DIAS"].ToString(),
+                        dr["MOTIVO"].ToString()
+
+                        ) ;
+                }
+            }
+
+            PRUEBAS_DATAGRID.Close();
+
+            gv_Compania.DataSource = GV;
+            gv_Compania.DataBind();
+        }
+       */
+        protected void gv_Compania_SelectedIndexChanged(object sender, EventArgs e)
         {
 
         }
+
+       
     }
 }
